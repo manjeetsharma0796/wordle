a6383777cdd8140b8a686bfb0b960fd4d13e595e
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-22 16:41:58 +0530

    Add: guess made, limited guess of 2, updated css

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-const guessOutcomeAndDisplay = (word, resultElement) => {
-  const guessedWordElement = document.querySelector("#guess-area");
-  if (guessedWordElement.value === word) {
-    resultElement.innerText = "Correct";
+const createGuessMadeElement = (guess) => {
+  const guessElement = document.createElement("p");
+  guessElement.innerText = guess;
+  return guessElement;
+};
+
+const disableGuessButton = (guessButton) => {
+  guessButton.disabled = true;
+};
+
+const guessOutcomeAndDisplay = (
+  guessedWord,
+  word,
+  resultElement,
+  chanceLeft,
+  guessButton
+) => {
+  resultElement.innerText = guessedWord === word ? "Correct" : "Incorrect";
+  chanceLeft.innerText = Number(chanceLeft.innerText) - 1;
+  if (Number(chanceLeft.innerText) === 0) {
+    disableGuessButton(guessButton);
   }
-  resultElement.innerText = "Incorrect";
 };
 
 window.onload = () => {
   const guessButton = document.querySelector("#guess-button");
   const resultElement = document.querySelector("#result");
   const word = "hello";
+  const chanceLeft = document.querySelector("#chance-left");
+  const guessContainer = document.querySelector("#guess-history");
+  const guessedWordElement = document.querySelector("#guess-area");
 
   guessButton.onclick = () => {
-    guessOutcomeAndDisplay(word, resultElement);
+    const guessedWord = guessedWordElement.value;
+    guessOutcomeAndDisplay(
+      guessedWord,
+      word,
+      resultElement,
+      chanceLeft,
+      guessButton
+    );
+
+    const guessElement = createGuessMadeElement(guessedWordElement.value);
+    guessContainer.appendChild(guessElement);
   };
 };
