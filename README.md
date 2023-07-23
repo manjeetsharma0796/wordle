64124eb32c4917c20cdf3d2adff3fd8260fcd57b
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-23 22:42:35 +0530

    Add: Class Player, Game, Controller, Renderer implement, Update: TODO.txt, README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Iteration 1:
 
 - Ask user to guess a 5 letter secret word picked by you and let user know if the guess is correct or not.
+
+Iteration 2:
+
+- Allow user to make 2 guesses and also show user the guesses the user has made.
+
+Iteration 3:
+
+- Inform user about number of correct letters present in the secret word.
--- index.js
+++ index.js
@@ -1,46 +1,113 @@
-const createGuessMadeElement = (guess) => {
-  const guessElement = document.createElement("p");
-  guessElement.innerText = guess;
-  return guessElement;
-};
+class Player {
+  #guessedHistory;
 
-const disableGuessButton = (guessButton) => {
-  guessButton.disabled = true;
-};
-
-const guessOutcomeAndDisplay = (
-  guessedWord,
-  word,
-  resultElement,
-  chanceLeft,
-  guessButton
-) => {
-  resultElement.innerText = guessedWord === word ? "Correct" : "Incorrect";
-  chanceLeft.innerText = Number(chanceLeft.innerText) - 1;
-  if (Number(chanceLeft.innerText) === 0) {
-    disableGuessButton(guessButton);
+  constructor() {
+    this.#guessedHistory = [];
   }
-};
+
+  registerGuess(guessedWord) {
+    this.#guessedHistory.push(guessedWord);
+  }
+
+  get guessHistory() {
+    return [...this.#guessedHistory];
+  }
+}
+
+class Game {
+  #player;
+  #correctWord;
+  #totalChance;
+  #chanceLeft;
+  #guessed;
+  constructor(player, word, totalChance) {
+    this.#player = player;
+    this.#correctWord = word;
+    this.#totalChance = totalChance;
+    this.#chanceLeft = totalChance;
+    this.#guessed = false;
+  }
+
+  isGuessCorrect(guessedWord) {
+    return guessedWord === this.#correctWord;
+  }
+
+  updateGame(guessedWord) {
+    this.#player.registerGuess(guessedWord);
+    this.#guessed = this.isGuessCorrect(guessedWord);
+    this.#chanceLeft = this.#chanceLeft - 1;
+  }
+
+  isGameOver() {
+    return this.#chanceLeft === 0 || this.#guessed ? true : false;
+  }
+
+  get status() {
+    const guessedHistory = [...this.#player.guessHistory];
+    const guessed = this.#guessed;
+    const chanceLeft = this.#chanceLeft;
+    return { guessed, guessedHistory, chanceLeft };
+  }
+}
+
+class Controller {
+  constructor(game, renderer) {
+    this.game = game;
+    this.renderer = renderer;
+  }
+
+  takeGuess(guessedWord) {
+    if (this.game.isGameOver()) {
+      return;
+    }
+
+    this.game.updateGame(guessedWord);
+    const status = this.game.status;
+    this.renderer.render(status);
+  }
+}
+
+class Renderer {
+  constructor() {
+    this.resultElement = document.querySelector("#result");
+    this.chancesLeft = document.querySelector("#chance-left");
+    this.guessedHistory = document.querySelector("#guess-history");
+  }
+
+  #renderGuessHistory(guessHistory) {
+    this.guessedHistory.replaceChildren();
+
+    guessHistory.forEach((guess) => {
+      const guessElement = document.createElement("p");
+      guessElement.innerText = guess;
+      this.guessedHistory.appendChild(guessElement);
+    });
+  }
+
+  render(status) {
+    const { guessed, guessedHistory, chanceLeft } = status;
+
+    this.resultElement.innerText = guessed ? "correct" : "incorrect";
+    this.chancesLeft.innerText = chanceLeft;
+    this.#renderGuessHistory(guessedHistory);
+  }
+}
 
 window.onload = () => {
   const guessButton = document.querySelector("#guess-button");
   const resultElement = document.querySelector("#result");
-  const word = "hello";
-  const chanceLeft = document.querySelector("#chance-left");
-  const guessContainer = document.querySelector("#guess-history");
   const guessedWordElement = document.querySelector("#guess-area");
+  const word = "hello";
+  const totalChance = 2;
+
+  const renderer = new Renderer(resultElement);
+  const player = new Player();
+
+  const game = new Game(player, word, totalChance);
+  const controller = new Controller(game, renderer);
 
   guessButton.onclick = () => {
     const guessedWord = guessedWordElement.value;
-    guessOutcomeAndDisplay(
-      guessedWord,
-      word,
-      resultElement,
-      chanceLeft,
-      guessButton
-    );
-
-    const guessElement = createGuessMadeElement(guessedWordElement.value);
-    guessContainer.appendChild(guessElement);
+    controller.takeGuess(guessedWord);
   };
 };
